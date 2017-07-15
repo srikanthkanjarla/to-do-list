1a2ef842765b4d830cb071a801afa6d23f45fb33
srikanth <srikanthkanjarla@gmail.com>
2017-07-15 12:25:20 +0530

    application functionality partially finished

--- js/default.js
+++ js/default.js
@@ -1,68 +1,53 @@
- $(function() {
-     var todo = {
-             getAllTasks: function() {
+ var todo = {
+         //date Object to get today, tomorrow, yesterday dates
+         dateObj: {
+             today: function() {
+                 var date = new Date();
+                 return this.format(date);
+             },
+             tomorrow: function() {
+                 var date = new Date();
+                 date.setDate(date.getDate() + 1);
+                 return this.format(date);
+             },
+             yesterday: function() {
+                 var date = new Date();
+                 date.setDate(date.getDate() - 1);
+                 return this.format(date);
+             },
+             //date format YYYY-MM-DD
+             format: function(date) {
+                 var day = (date.getDate()) < 10 ? "0" + (date.getDate()) : (date.getDate());
+                 var month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+                 var year = date.getFullYear();
+                 return (year + "-" + month + "-" + day);
+             }
+         }, // END of date Object
+
+         //setTaskList Object (container for set methods)to add new tasks and to set properties of tasks
+         setTaskList: {
+             addNewTask: function(taskObj) {
                  $.ajax({
                      type: "POST",
-                     url: "http://srikanth/php_work/to_do_list/todo.php",
-                     data: {
-                         query: "getTasks"
-                     },
-                     cache: false,
-                     success: function(result) {
-                         todo.displayAllTasks(result);
-                     }
-                 });
-             },
-             displayAllTasks: function(tasks) {
-                 var all_tasks = JSON.parse(tasks);
-                 var tasks_count = (Object.keys(all_tasks).length);
-                 $('#task_count').text("Total Tasks :" + tasks_count);
-
-                 for (var task_key in all_tasks) {
-                     var task_name = (all_tasks[task_key].task_name);
-                     var task_status = (all_tasks[task_key].task_status);
-                     //console.log(task_status);
-                     var tasks_out;
-                     if (task_status == 2) {
-                         tasks_out = "<p id='task" + task_key + "'><input type='checkbox' checked id='status" + task_key + "'> <label class='strike' id='label" + task_key + "' for='task" + task_key + "' title='Finished Task'>" + task_name + "</label></input>"
-                     } else {
-                         tasks_out = "<p id='task" + task_key + "'><input type='checkbox' id='status" + task_key + "'> <label id='label" + task_key + "' for='task" + task_key + "' title='Finish'>" + task_name + "</label></input>"
-                     }
-
-                     tasks_out += " <span  class='pull-right change'>";
-                     tasks_out += "<a href='#' class='edit' id='edit" + task_key + "' title='Edit'><span class='glyphicon glyphicon-pencil'></span></a> &nbsp;&nbsp;";
-                     tasks_out += "<a href='#' class='delete'id='delete" + task_key + "' title='Delete'><span class='glyphicon glyphicon-remove'></span></a>";
-                     tasks_out += "</span></p>";
-
-                     $('.todo_tasks').append(tasks_out);
-                 }
-             },
-             addNewTask: function(task) {
-                 //var task = $('.add input').val();
-                 $.ajax({
-                     type: "POST",
-                     url: "http://srikanth/php_work/to_do_list/todo.php",
+                     url: "http://srikanth/php-work/to-do-list/todo.php",
                      data: {
                          query: "insert",
-                         todo: task
+                         task: taskObj.task,
+                         date: taskObj.date
                      },
                      cache: false,
                      success: function(result) {
-                         $(".add input").val('');
-                         $(".todo_tasks").html('');
-                         todo.getAllTasks();
+                         alert("Todo added Successfully");
+
+                         // console.log(result);
+
                      }
                  });
              },
-
              editTaskName: function(edit_task) {
-
-                 //var task = $("#edit-task" + id).val();
-                 //var edit_task = $("#edit-task" + id).val();
-                 console.log(edit_task);
                  $.ajax({
                      type: "POST",
-                     url: "http://srikanth/php_work/to_do_list/todo.php",
+                     url: "http://srikanth/php-work/to-do-list/todo.php",
                      data: {
                          query: "edit",
                          data_obj: edit_task
@@ -70,19 +55,20 @@
                      cache: false,
                      success: function(response) {
                          console.log(response);
-                         $(".todo_tasks").html('');
+                         $(".todo-tasks").html('');
                          todo.getAllTasks();
+                         alert("Todo name updated successfully");
                      }
                  });
 
              },
-
-             deleteTask: function(id) {
+             // hide active task from listing by setting visibility = false
+             setTaskHidden: function(id) {
                  var key = id.replace(/^\D+/g, '');
                  alert('Are you sure - delete Task :' + key);
                  $.ajax({
                      type: "POST",
-                     url: "http://srikanth/php_work/to_do_list/todo.php",
+                     url: "http://srikanth/php-work/to-do-list/todo.php",
                      data: {
                          query: "delete",
                          id: key
@@ -90,15 +76,19 @@
                      cache: false,
                      success: function(response) {
                          console.log(response);
-                         $(".todo_tasks").html('');
                          todo.getAllTasks();
                      }
                  });
              },
-             updateTaskStatus: function(key) {
+             //set hidden task as active by setting visibility = true
+             setTaskAsActive: function() {
+
+             },
+             //mark task as complete 
+             setTaskAsComplete: function(key) {
                  $.ajax({
                      type: "POST",
-                     url: "http://srikanth/php_work/to_do_list/todo.php",
+                     url: "todo.php",
                      data: {
                          query: "status",
                          id: key
@@ -106,36 +96,142 @@
                      cache: false,
                      success: function(response) {
                          console.log(response);
-                         $(".todo_tasks").html('');
-                         todo.getAllTasks();
                      }
                  });
              }
-         } //end of todo object
+         }, //END of setTaskList Object
+
+         // getTaskList Object to fetch tasks to display 
+         getTaskList: {
+
+             displayTasks: function(taskObj, viewClass) {
+                 var tasks = JSON.parse(taskObj);
+                 var currentDate = false;
+                 for (key in tasks) {
+                     var taskItem = "";
+                     var taskHeading = "";
+                     if (tasks.hasOwnProperty(key)) {
+                         var taskId = (tasks[key].id);
+                         var taskName = (tasks[key].task_name);
+                         var taskStatus = (tasks[key].task_status);
+                         var taskDate = (tasks[key].date_added);
+                         var taskVisibility = (tasks[key].visibility);
+                     }
+
+                     if (taskStatus == 2 && taskVisibility == 2) {
+                         taskItem += "<li class='text-warning'><label id='label" + taskId + "' for='" + taskId + "'>" + taskName + "</label>";
+
+                     } else if (taskStatus == 2 && taskVisibility == 1) {
+                         taskItem += "<li class='text-success'><label class='strike' id='label" + taskId + "' for='" + taskId + "'> <input type='checkbox' checked='checked' id='" + taskId + "'> </input>" + taskName + "</label>";
+                     } else {
+                         taskItem += "<li class='text-primary'><label id='label" + taskId + "' for='" + taskId + "'> <input type='checkbox' id='" + taskId + "'> </input>" + taskName + "</label>";
+                     }
+                     if (taskStatus == 1 || taskVisibility == 1) {
+                         taskItem += " <span class='pull-right change'>";
+                         taskItem += "<a href='#' class='edit' id='edit" + taskId + "' title='Edit'><span class='glyphicon glyphicon-pencil'></span></a> &nbsp;&nbsp;";
+                         taskItem += "<a href='#' class='delete'id='delete" + taskId + "' title='Delete'><span class='glyphicon glyphicon-trash text-danger'></span></a>";
+                         taskItem += "</span></li>";
+                     }
+                     if (taskStatus == 2 && taskVisibility == 2) {
+                         taskItem += " <span class='pull-right change'>";
+                         taskItem += "<a href='#' class='delete'id='delete" + taskId + "' title='restore'><span class='text-danger'> Restore</span></a></span></li>";
+                     }
 
 
-     // calling functions 
+                     if (viewClass === 'today-tasks') {
+                         $('.today-tasks').append(taskItem);
+                     }
+                     if (viewClass === 'yesterday-tasks') {
+                         $('.yesterday-tasks').append(taskItem);
+                     }
+                     if (viewClass === 'tomorrow-tasks') {
+                         $('.tomorrow-tasks').append(taskItem);
+                     }
 
-     // 1. Get tasks on initial page load
+                     if (taskDate !== currentDate) {
+                         taskHeading += "<h5 class='group-head'>" + taskDate + "</h5>";
+                         currentDate = taskDate;
+                     }
 
-     todo.getAllTasks();
+                     if (viewClass === 'active-tasks') {
+                         $('.active-tasks').append(taskHeading + taskItem);
+                     }
+                     if (viewClass === 'done-tasks') {
+                         $('.done-tasks').append(taskHeading + taskItem);
+                     }
+                     if (viewClass === 'deleted-tasks') {
+                         $('.deleted-tasks').append(taskHeading + taskItem);
+                     }
 
-     // 2. Add New Task on pressing Enter key
-     $(".add").find('input').keypress(function(e) {
-         if (e.which == 10 || e.which == 13) {
-             var task = $('.add input').val();
-             todo.addNewTask(task);
+                 }
+             },
+
+             getTasks: function(query, date, viewClass) {
+                 $.ajax({
+                     type: "POST",
+                     url: "http://srikanth/php-work/to-do-list/todo.php",
+                     data: {
+                         query: query,
+                         date: date,
+                     },
+                     cache: false,
+                     success: function(response) {
+                         todo.getTaskList.displayTasks(response, viewClass);
+                         console.log(typeof(response));
+                     }
+                 });
+             },
+         }, //END of getTaskList Object         
+     } //END of todo object
+
+ // calling methods  
+ $(function() {
+
+     //jqueryUI date picker 
+     $("#date").datepicker({ dateFormat: 'yy-mm-dd' });
+
+     // getTaskList methods
+     todo.getTaskList.getTasks('today', todo.dateObj.today(), 'today-tasks');
+     todo.getTaskList.getTasks('yesterday', todo.dateObj.yesterday(), 'yesterday-tasks');
+     todo.getTaskList.getTasks('tomorrow', todo.dateObj.tomorrow(), 'tomorrow-tasks');
+     todo.getTaskList.getTasks('active', todo.dateObj.tomorrow(), 'active-tasks');
+     todo.getTaskList.getTasks('done', todo.dateObj.tomorrow(), 'done-tasks');
+     todo.getTaskList.getTasks('deleted', todo.dateObj.tomorrow(), 'deleted-tasks');
+     //todo.setTaskList.addNewTask()
+     $("#add-task").on('submit', function(e) {
+         e.preventDefault();
+         e.defaultPrevented;
+         var data = {
+             task: $('#task').val(),
+             date: $("#date").val()
          }
+         todo.setTaskList.addNewTask(data);
+         $("#add_task").reset();
+         return false;
      });
 
+     //delete task 
+     $('.todo-tasks').on('click', '.delete', function(e) {
+         var id = $(this).attr('id');
+         // todo.deleteTask(id);
+         e.preventDefault();
+     });
 
+     //Update task status
+     $(".todo-tasks").on('click', 'input:checkbox', function(e) {
+         var key = $(this).attr('id');
+         $("#label" + key).toggleClass('strike');
+         //$(this).parent().toggleClass('strike');  
+         // todo.markAsComplete(key);
+     });
 
      //edit taks name
-     $('.todo_tasks').on('click', '.edit', function() {
+     $('.todo_tasks').on('click', '.edit', function(e) {
          var id = $(this).attr('id');
          var key = id.replace(/^\D+/g, '');
          var label = $('#label' + key).text();
          var ele = $('#task' + key).html();
+         e.preventDefault();
          //$('#task' + key).html(ele);
          $('#task' + key).html(' <input type="text" class="form-control capital-bold" id="edit-task' + key + '" name="task" value="">');
          $('#edit-task' + key).focus().val(label);
@@ -147,24 +243,8 @@
                          id: key
                      }
                      //console.log(edit_task);
-                 todo.editTaskName(edit_task);
+                     //todo.editTaskName(edit_task);
              }
          });
-
-
      })
-
-     //delete task 
-     $('.todo_tasks').on('click', '.delete', function() {
-         var id = $(this).attr('id');
-         todo.deleteTask(id);
-     });
-
-     //Update task status
-     $(".todo_tasks").on('click', 'input:checkbox', function() {
-         //alert('hey! you checked a checkbox');
-         var id = $(this).attr('id');
-         var key = id.replace(/^\D+/g, '');
-         todo.updateTaskStatus(key);
-     });
  });
--- todo.php
+++ todo.php
@@ -1,7 +1,7 @@
 <?php
 class Todo
 {
-    public $mysql; // to store database conneciton handle
+    private $mysql; // to store database conneciton handle
 
  // connect to database
     function __construct()
@@ -10,78 +10,81 @@ class Todo
     }
 
  //Add new todo to database
-    function insert_task($task)
-    {
-        $status = 1;  //by default every task is unfinished (status: 1 = unfinished  2 = finished)
-        $query = "INSERT INTO todo(task,status)  VALUES ('$task','$status')";
+    function addNewTodo($data)
+    {   
+        if(!empty($data[0]) && !empty($data[1]))
+        {
+        $query = "INSERT INTO todo(task,date_added)  VALUES ('$data[0]','$data[1]')";
         $result = $this->mysql->query($query) or die($this->mysql->error);
         return $result;
+        }
     }
 
  //update existing todo task name in database 
-    function update_task($data)
+    function updateTodoName($data)
     {
-        
         $query = "UPDATE todo SET task='$data[task_name]' WHERE id=$data[task_id]";
         $result =$this->mysql->query($query) or die($this->mysql->error);
         return $result;
     }
 
-//delete exisiting todo task from database
-    function delete_task($id)
+//delete exisiting todo task visibility hide/show
+    function updateTodoVisibility($id)
     {
-        $query ="DELETE FROM todo WHERE id=$id";
-        $result = $this->mysql->query($query) or die($this ->mysql->error);
-        return $result;
+         $query = "SELECT visibility FROM todo WHERE id = $id";
+        $result = $this->mysql->query($query) or die($this->mysql->error); 
+        $visibility='';
+        while($row = $result->fetch_object())
+          {
+          $visibility = $row->visibility;
+          }
+          $visibility = $visibility == 1?2:1;
+          $q = "UPDATE todo SET visibility = $visibility AND status=2 WHERE id=$id";                            
+          $res  = $this->mysql->query($q) or die($this ->mysql->error);
+          return $res;
     }
-//update exisiting todo task status in database
-    function update_task_status($id)
+//update exisiting todo task status finished/unfinished
+    function updateTodoStatus($id)
     {
         $query = "SELECT status FROM todo WHERE id = $id";
-        $result = $this->mysql->query($query) or die($db->mysql->error); 
-        $task_status;
+        $result = $this->mysql->query($query) or die($this->mysql->error); 
+        $taskStatus;
         while($row = $result->fetch_object())
-                            {
-                                $task_status = $row->status;
-                            }
-        if($task_status == 1)
-        {
-         $q = "UPDATE todo SET status = 2 WHERE id=$id";
-        }
-        elseif($task_status==2)
-        {
-             $q = "UPDATE todo SET status = 1 WHERE id=$id";
-        }
-        //$query ="DELETE FROM todo WHERE id=$id";
-        $res  = $this->mysql->query($q) or die($this ->mysql->error);
-        return $res;
+          {
+          $taskStatus = $row->status;
+          }
+          $taskStatus = $taskStatus == 1?2:1;
+          $q = "UPDATE todo SET status = $taskStatus WHERE id=$id";                            
+          $res  = $this->mysql->query($q) or die($this ->mysql->error);
+          return $res;
     }
 
-// read all todo tasks from database
-    function get_tasks()
-    {
-                        //$db = new Db();
-                        $query = "SELECT * FROM todo ORDER BY id ASC";
-                        $results = $this->mysql->query($query) or die($db->mysql->error); 
+//get tasks from DB based on query
+ 
+    function getAllTasks($query)
+    {                                                                  
+                        $results = $this->mysql->query($query) or die($this->mysql->error); 
                         if($results->num_rows)
                         {
                           $tasks_count = $results -> num_rows;   
                           $tasks_count==1 ?$tasks_count.="<span> Task to do </span>" :$tasks_count.= "<span> Tasks to do</span>";
-                          $rows = array();
+                         $rows =[];                          
                           while($row = $results->fetch_object())
-                            {
+                            {                                
                                 $task_id     = $row -> id;
                                 $task_title  = $row->task;
-                                $task_status = $row->status; 
-                                $rows[$task_id] = ["task_name"=>$task_title,"task_status"=>$task_status];
-                            }
+                                $task_status = $row->status;
+								$task_added_on=$row->date_added;
+                                $task_visibility=$row->visibility;
+                                $rows[$task_id] = ["id"=>$task_id,"task_name"=>$task_title,"task_status"=>$task_status,"date_added"=>$task_added_on,"visibility"=>$task_visibility];
+                            }    
                             return json_encode($rows);
                          }
-                         else
-                         {
-                           return "No tasks to do"; 
-                         }
-    }
+                         //else
+                         //{
+                          //return "No tasks to do! (:-|)"; 
+                         //}
+    }      
 } // End of class
 
 $task_obj = new Todo(); // new object to Todo class
@@ -89,70 +92,129 @@ $task_obj = new Todo(); // new object to Todo class
 // call insert_task() which Add new todo tasks to database
 if(isset($_POST['query']) && $_POST['query'] == 'insert')
 {
-    $todo = $_POST['todo'];
-    $res=$task_obj->insert_task($todo);
+    $task = $_POST['task'];
+    $date = $_POST['date'];
+    $data=[$task,$date];
+    $res=$task_obj->addNewTodo($data);
     if($res){
-        echo "Todo added sucessfully";
+        echo "Todo added sucessfully "; 
     }
 }
 
-//call get_tasks() which get all tasks from database
-if(isset($_POST['query']) && $_POST['query'] == 'getTasks')
+//get all active tasks 
+if(isset($_POST['query']) && $_POST['query'] == 'active')
 {
-    $res = $task_obj ->get_tasks();
+    //$date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE status = 1 ORDER BY id DESC" ;
+   $res = $task_obj ->getAllTasks($query);
     if($res)
     {
-        //print_r($res);
-        echo $res;
+       //print_r($res);
+        echo $res;        
     }
 }
+ 
+//get all finished (done) tasks 
+if(isset($_POST['query']) && $_POST['query'] == 'done')
+{
+    //$date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE status = 2 AND  visibility = 1 ORDER BY id DESC" ;
+   $res = $task_obj ->getAllTasks($query);
+    if($res)
+    {        
+        echo $res;        
+    }
+}
+
+//get all deleted (not visible) tasks 
+if(isset($_POST['query']) && $_POST['query'] == 'deleted')
+{
+    //$date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE visibility = 2 ORDER BY id DESC" ;
+   $res = $task_obj ->getAllTasks($query);
+    if($res)
+    {        
+        echo $res;        
+    }
+}
+
+//get tasks of today  
+if(isset($_POST['query']) && $_POST['query'] == 'today')
+{
+    $date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
+    $res = $task_obj ->getAllTasks($query);
+    if($res)
+    {
+        echo $res;   
+    }
+}
+
+//get all tasks of tomorrow
+if(isset($_POST['query']) && $_POST['query'] == 'tomorrow')
+{
+    $date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
+    $res = $task_obj ->getAllTasks($query);
+    if($res)
+    {
+        echo $res;   
+    }
+}
+
+//get all tasks of yesterday
+if(isset($_POST['query']) && $_POST['query'] == 'yesterday')
+{
+    $date = $_POST['date'];
+     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
+    $res = $task_obj ->getAllTasks($query);
+    if($res)
+    {
+        echo $res;   
+    }
+}
+
 // edit task
 if(isset($_POST['query']) && $_POST['query'] =='edit')
 {
     $task_name = $_POST['data_obj']['task_name'];
     $task_id = $_POST['data_obj']['id'];
     $data = ['task_id'=>$task_id,'task_name'=>$task_name];
-    $res = $task_obj -> update_task($data);
+    $res = $task_obj -> updateTodoName($data);
     if($res)
     {
-        print_r($res);
+        //print_r($res);
 echo "Task Name Updated Successfully!";
-    }
+    }/*
     else{
         echo "Oops something wrong Failed to update ";
-    }
-     
-   
-    
+    }*/   
 }
 
 // delete task
 if(isset($_POST['query']) && $_POST['query'] =='delete')
 {
     $id = $_POST['id'];
-    $res = $task_obj -> delete_task($id);
+    $res = $task_obj -> updateTodoVisibility($id);
     if($res)
     {
 echo "Task deleted Successfully! $id";
-    }
+    }/*
     else{
         echo "Oops something wrong ";
-    }
-    
+    }*/
 }
 //status update
 if(isset($_POST['query']) && $_POST['query'] =='status')
 {
     $id = $_POST['id'];
-    $res = $task_obj -> update_task_status($id);
+    $res = $task_obj -> updateTodoStatus($id);
     if($res)
     {
     echo "Task status updated Successfully! $res";
-    }
+    }/*
     else{
         echo "Oops something wrong ";
-    }
-    
-    
-}
+    }*/   
+} 
 ?>
