ae89bc638a8a54811c3e57d8cbd5958930bab4cc
srikanth <srikanthkanjarla@gmail.com>
2017-10-20 20:03:19 +0530

    complete rewrite of code

--- README.md
+++
@@ -1,2 +0,0 @@
-# to-do-list
-Simple to do list application using PHP,MySql, HTML, CSS, BOOTSTRAP to organise tasks and projects - Learning purpose
--- todo.php
+++ todo.php
@@ -1,220 +1,167 @@
 <?php
-class Todo
-{
-    private $mysql; // to store database conneciton handle
+class Todo{
+/*
+-- connect to database
+-- add items to databse
+-- get items from database
+-- update status
+-- update visibility
+*/
 
- // connect to database
-    function __construct()
-    {
-    $this->mysql = new mysqli('localhost','root','','to_do_list') or die('Unable to connect to Db');
+  private $user='root';
+  private $host='localhost';
+  private $password='sri';
+  private $db='todo_db';  
+  private $con;
+// connect to database
+function __construct(){
+    $this->con = new mysqli($this->host,$this->user,$this->password,$this->db);  
+    if(!$this->con->connect_error){
+       // echo "connection established";
     }
-
- //Add new todo to database
-    function addNewTodo($data)
-    {   
-        if(!empty($data[0]) && !empty($data[1]))
-        {
-        $query = "INSERT INTO todo(task,date_added)  VALUES ('$data[0]','$data[1]')";
-        $result = $this->mysql->query($query) or die($this->mysql->error);
-        return $result;
-        }
-    }
-
- //update existing todo task name in database 
-    function updateTodoName($data)
-    {
-        $query = "UPDATE todo SET task='$data[task_name]' WHERE id=$data[task_id]";
-        $result =$this->mysql->query($query) or die($this->mysql->error);
-        return $result;
-    }
-
-//delete exisiting todo task visibility hide/show
-    function updateTodoVisibility($id)
-    {
-         $query = "SELECT visibility FROM todo WHERE id = $id";
-        $result = $this->mysql->query($query) or die($this->mysql->error); 
-        $visibility='';
-        while($row = $result->fetch_object())
-          {
-          $visibility = $row->visibility;
-          }
-          $visibility = $visibility == 1?2:1;
-          $q = "UPDATE todo SET visibility = $visibility AND status=2 WHERE id=$id";                            
-          $res  = $this->mysql->query($q) or die($this ->mysql->error);
-          return $res;
-    }
-//update exisiting todo task status finished/unfinished
-    function updateTodoStatus($id)
-    {
-        $query = "SELECT status FROM todo WHERE id = $id";
-        $result = $this->mysql->query($query) or die($this->mysql->error); 
-        $taskStatus;
-        while($row = $result->fetch_object())
-          {
-          $taskStatus = $row->status;
-          }
-          $taskStatus = $taskStatus == 1?2:1;
-          $q = "UPDATE todo SET status = $taskStatus WHERE id=$id";                            
-          $res  = $this->mysql->query($q) or die($this ->mysql->error);
-          return $res;
-    }
-
-//get tasks from DB based on query
- 
-    function getAllTasks($query)
-    {                                                                  
-                        $results = $this->mysql->query($query) or die($this->mysql->error); 
-                        if($results->num_rows)
-                        {
-                          $tasks_count = $results -> num_rows;   
-                          $tasks_count==1 ?$tasks_count.="<span> Task to do </span>" :$tasks_count.= "<span> Tasks to do</span>";
-                         $rows =[];                          
-                          while($row = $results->fetch_object())
-                            {                                
-                                $task_id     = $row -> id;
-                                $task_title  = $row->task;
-                                $task_status = $row->status;
-								$task_added_on=$row->date_added;
-                                $task_visibility=$row->visibility;
-                                $rows[$task_id] = ["id"=>$task_id,"task_name"=>$task_title,"task_status"=>$task_status,"date_added"=>$task_added_on,"visibility"=>$task_visibility];
-                            }    
-                            return json_encode($rows);
-                         }
-                         //else
-                         //{
-                          //return "No tasks to do! (:-|)"; 
-                         //}
-    }      
-} // End of class
-
-$task_obj = new Todo(); // new object to Todo class
- 
-// call insert_task() which Add new todo tasks to database
-if(isset($_POST['query']) && $_POST['query'] == 'insert')
-{
-    $task = $_POST['task'];
-    $date = $_POST['date'];
-    $data=[$task,$date];
-    $res=$task_obj->addNewTodo($data);
-    if($res){
-        echo "Todo added sucessfully "; 
-    }
-}
-
-//get all active tasks 
-if(isset($_POST['query']) && $_POST['query'] == 'active')
-{
-    //$date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE status = 1 ORDER BY id DESC" ;
-   $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {
-       //print_r($res);
-        echo $res;        
-    }
-}
- 
-//get all finished (done) tasks 
-if(isset($_POST['query']) && $_POST['query'] == 'done')
-{
-    //$date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE status = 2 AND  visibility = 1 ORDER BY id DESC" ;
-   $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {        
-        echo $res;        
-    }
-}
-
-//get all deleted (not visible) tasks 
-if(isset($_POST['query']) && $_POST['query'] == 'deleted')
-{
-    //$date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE visibility = 2 ORDER BY id DESC" ;
-   $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {        
-        echo $res;        
-    }
-}
-
-//get tasks of today  
-if(isset($_POST['query']) && $_POST['query'] == 'today')
-{
-    $date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
-    $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {
-        echo $res;   
-    }
-}
-
-//get all tasks of tomorrow
-if(isset($_POST['query']) && $_POST['query'] == 'tomorrow')
-{
-    $date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
-    $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {
-        echo $res;   
-    }
-}
-
-//get all tasks of yesterday
-if(isset($_POST['query']) && $_POST['query'] == 'yesterday')
-{
-    $date = $_POST['date'];
-     $query ="SELECT * FROM todo WHERE date_added = '$date' ORDER BY id DESC" ;
-    $res = $task_obj ->getAllTasks($query);
-    if($res)
-    {
-        echo $res;   
-    }
-}
-
-// edit task
-if(isset($_POST['query']) && $_POST['query'] =='edit')
-{
-    $task_name = $_POST['data_obj']['task_name'];
-    $task_id = $_POST['data_obj']['id'];
-    $data = ['task_id'=>$task_id,'task_name'=>$task_name];
-    $res = $task_obj -> updateTodoName($data);
-    if($res)
-    {
-        //print_r($res);
-echo "Task Name Updated Successfully!";
-    }/*
     else{
-        echo "Oops something wrong Failed to update ";
-    }*/   
+        exit('Failed to connect');
+    }
 }
 
-// delete task
-if(isset($_POST['query']) && $_POST['query'] =='delete')
+private function sanitize_input($data){
+    $data = trim($data);
+    $data=$this->con->escape_string($data);
+    return $data;
+}
+
+//add items to todo table in todo_db database
+
+public function addNewTodo($data){
+  $data = $this->sanitize_input($data);
+  $sql = "INSERT INTO todo (task,visibility,status) VALUES ('$data',1,1)";
+  $res = $this->con->query($sql);
+  if(!$res){
+      die($this->con->error);
+  }
+  else{
+    echo "task added successfully" ;
+  }
+}
+
+public function updateTodoStatus($id)
 {
-    $id = $_POST['id'];
-    $res = $task_obj -> updateTodoVisibility($id);
-    if($res)
+    $query = "SELECT status FROM todo WHERE id = $id";
+    $result = $this->con->query($query) or die($this->con->error); 
+    $taskStatus='';
+    while($row = $result->fetch_object())
+      {
+      $taskStatus = $row->status;
+      }
+      $taskStatus = $taskStatus == 1?2:1;
+      $q = "UPDATE todo SET status = '$taskStatus' WHERE id=$id";                            
+      $res  = $this->con->query($q) or die($this ->con->error);
+     // return $res;
+     return "status".$taskStatus."id-".$id;
+}
+
+//get items from database
+public function getTodos($filter){
+    /*
+    switch($filter)
     {
-echo "Task deleted Successfully! $id";
-    }/*
-    else{
-        echo "Oops something wrong ";
+        case "all":
+        $sql = "SELECT * FROM todo  ORDER BY id DESC";
+        break;
+        case "active":
+        $sql = "SELECT * FROM todo WHERE status=0 ORDER BY id DESC";
+        break;
+        case "completed":
+        $sql = "SELECT * FROM todo WHERE status=1 ORDER BY id DESC";
+        break;
     }*/
-}
-//status update
-if(isset($_POST['query']) && $_POST['query'] =='status')
-{
-    $id = $_POST['id'];
-    $res = $task_obj -> updateTodoStatus($id);
-    if($res)
-    {
-    echo "Task status updated Successfully! $res";
-    }/*
-    else{
-        echo "Oops something wrong ";
-    }*/   
+    if($filter == 'all'){
+        $sql = "SELECT * FROM todo  WHERE visibility=1 ORDER BY id DESC";
+    }
+    if($filter == 'active'){
+        $sql = "SELECT * FROM todo WHERE status=1 AND visibility=1 ORDER BY id DESC";   
+    }
+    if($filter == 'completed'){
+        $sql = "SELECT * FROM todo WHERE status=2 AND visibility=1 ORDER BY id DESC";
+    }
+$res = $this->con->query($sql);
+$tasksArr=array();
+if($res){
+    while ($row = $res->fetch_assoc()) {
+    $tasksArr[]=$row;
+    }
+    return json_encode($tasksArr);
 } 
-?>
+ 
+}
+
+ 
+
+//update todo task visibility
+public function updateTodoVisibility($id){
+    $query = "SELECT visibility FROM todo WHERE id = $id";
+    $result = $this->con->query($query) or die($this->con->error); 
+    $taskStatus='';
+    while($row = $result->fetch_object())
+      {
+        $taskVisibility = $row->visibility;
+      }
+      $taskVisibility = $taskVisibility == 1?2:1;
+      $visb = "UPDATE todo SET visibility = '$taskVisibility' WHERE id=$id";                            
+      $visbRes  = $this->con->query($visb) or die($this ->con->error);
+     // return $res;
+     return "task deleted". $id;
+}
+}
+
+
+$obj = new Todo();
+ 
+if(isset($_POST["action"]) && $_POST["action"] == 'addNewTask'){
+    //echo "Task Added successfully";
+    
+    $obj->addNewTodo($_POST['taskTitle']);
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == 'allTasks'){
+    $allTAsks = $obj->getTodos('all');
+    echo $allTAsks;
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == 'activeTasks'){
+    $allTAsks = $obj->getTodos('active');
+    echo $allTAsks;
+     
+
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == 'completedTasks'){
+    $allTAsks = $obj->getTodos('completed');
+    echo $allTAsks;
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == 'updateStatus'){
+    //echo "Task Added successfully";
+    $id=trim($_POST['id']);
+    if(filter_var($id, FILTER_VALIDATE_INT)){
+       $resp = $obj->updateTodoStatus($id);
+       print_r($resp);
+    }
+    else{
+        echo "incorrect task id";
+    }
+    
+}
+if(isset($_POST["action"]) && $_POST["action"] == 'updateVisibility'){
+    //echo "Task Added successfully";
+    $id=trim($_POST['id']);
+    if(filter_var($id, FILTER_VALIDATE_INT)){
+       $resp = $obj->updateTodoVisibility($id);
+       print_r($resp);
+    }
+    else{
+        echo "incorrect task id";
+    }
+    
+}
+
